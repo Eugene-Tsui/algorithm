e6ab0285d0075911f19c169e5de2ad9b7124475b
cyz <2276290611@qq.com>
2019-02-21 22:15:00 +0800

    PAT1087

---
+++ PAT/T2.java
@@ -0,0 +1,33 @@
+package Test1;
+
+import java.util.*;
+
+public class T2 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		List list = new ArrayList();
+		int a=-1;
+		for(int i=1;i<=n;i++) {
+			if(a==i/2+i/3+i/5)
+				continue;
+			else {
+				list.add(i/2+i/3+i/5);
+				a=i/2+i/3+i/5;
+				System.out.println(i/2+i/3+i/5);
+			}
+		}
+		//removeDuplicate(list);
+		System.out.print(list.size());
+	}
+	public   static  List removeDuplicate(List list)  {       
+		  for  ( int  i  =   0 ; i  <  list.size()  -   1 ; i ++ )  {       
+		      for  ( int  j  =  list.size()  -   1 ; j  >  i; j -- )  {       
+		           if  (list.get(j).equals(list.get(i)))  {       
+		              list.remove(j);       
+		            }        
+		        }        
+		      }        
+		    return list;       
+		}
+}
