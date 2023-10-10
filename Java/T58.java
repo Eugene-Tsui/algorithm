87acf96e75e5860bc048941e7b8617f6ad37d77e
cuiyzh <2276290611@qq.com>
2023-10-10 10:02:37 +0800

    题解上传

---
+++ Java/T58.java
@@ -0,0 +1,10 @@
+package Java;
+
+public class T58 {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("Hello World"));
+    }
+    public static int lengthOfLastWord(String s) {
+        return 0;
+    }
+}
