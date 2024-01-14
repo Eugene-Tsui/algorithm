fa7a98afd2bbb142cb25a0d9ed9d233001f6de9c
cuiyzh <2276290611@qq.com>
2024-01-14 20:38:45 +0800

    题解

---
+++ Java/T136.java
@@ -0,0 +1,20 @@
+package Java;
+
+import java.util.HashSet;
+
+public class T136 {
+    public static void main(String[] args) {
+        int[] nums = {2,2,1};
+        System.out.println(singleNumber(nums));
+
+    }
+    public static int singleNumber(int[] nums) {
+        int singNum = 0;
+        int len = nums.length;
+        for (int i = 0; i < len; ++i) {
+            singNum = singNum ^ nums[i];
+        }
+        return singNum;
+
+    }
+}
